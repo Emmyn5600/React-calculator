0e8c3b96064a6fe50c7fbe5bbe9d5ab3fe682016
emmyn5600 <emmy6618@gmail.com>
2021-07-23 15:15:58 +0200

    Modify the app.js file

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,13 @@
-/* eslint-disable react/no-access-state-in-setstate */
 import React from 'react';
 import Display from './Display';
 import ButtonPannel from './ButtonPanel';
-import calculate from '../logic/calculator';
+import calculator from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
+const App = () => (
+  <>
+    <Display />
+    <ButtonPannel />
+  </>
+);
 
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(buttonName) {
-    const calculation = calculate(buttonName, this.state);
-    this.setState({
-      total: calculation.total,
-      next: calculation.next,
-      operation: calculation.operation,
-    });
-  }
-
-  render() {
-    const { total, next } = this.state;
-    return (
-      <>
-        <Display calculation={next || total} />
-        <ButtonPannel clickHandler={(e) => this.handleClick(e.target.innerText)} />
-      </>
-    );
-  }
-}
 export default App;
