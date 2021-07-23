5530d421d38b74fdb97e362818913b0c1864ec64
emmyn5600 <emmy6618@gmail.com>
2021-07-23 14:35:11 +0200

    Work on the milestone-4 to implement logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,38 @@
+/* eslint-disable react/no-access-state-in-setstate */
 import React from 'react';
 import Display from './Display';
 import ButtonPannel from './ButtonPanel';
+import calculate from '../logic/calculator';
 
-const App = () => (
-  <>
-    <Display />
-    <ButtonPannel />
-  </>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
 
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const calculation = calculate(buttonName, this.state);
+    this.setState({
+      total: calculation.total,
+      next: calculation.next,
+      operation: calculation.operation,
+    });
+  }
+
+  render() {
+    const { total, next } = this.state;
+    return (
+      <>
+        <Display calculation={next || total} />
+        <ButtonPannel clickHandler={(e) => this.handleClick(e.target.innerText)} />
+      </>
+    );
+  }
+}
 export default App;
