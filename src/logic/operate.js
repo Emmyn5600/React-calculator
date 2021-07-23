7719659f04f74002efc1f370e355b5a93b9461ec
emmyn5600 <emmy6618@gmail.com>
2021-07-23 10:54:57 +0200

    Add the opearate.js file with the logic

---
+++ src/logic/operate.js
@@ -0,0 +1,30 @@
+import Big from './big';
+
+const operate = (numberOne, numberTwo, operation) => {
+  let result = 0;
+  const num1 = new Big(numberOne);
+  const num2 = new Big(numberTwo);
+
+  switch (operation){
+      case "X":
+      result = num1.times(num2);
+      break;
+      case "+":
+      result = num1.plus(num2);
+      break;
+      case "-":
+      result = num1.minus(num2);
+      break;
+      case '÷':
+      result = num1.div(num2);
+      break;
+      case '%':
+      result = num1.times(0.1);
+      break;
+      default:
+      break;
+  }
+    return result.toString();
+};
+
+export default operate;
