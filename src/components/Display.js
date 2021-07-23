3e1e9b17000173a24356694370e1702a12c952b3
emmyn5600 <emmy6618@gmail.com>
2021-07-23 09:48:31 +0200

    Add the Display.js

---
+++ src/components/Display.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Display = ({ result }) => <div className="display-result">{result}</div>;
+
+Display.defaultProps = {
+  result: '0',
+};
+
+Display.propTypes = {
+  result: PropTypes.string,
+};
+
+export default Display;
