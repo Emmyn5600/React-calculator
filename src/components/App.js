ed92f19f37797cc6f1402e588b94d2cadf9d4f29
emmyn5600 <emmy6618@gmail.com>
2021-07-23 21:24:39 +0200

    import calculate.js as suggested

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import Display from './Display';
 import ButtonPannel from './ButtonPanel';
+import calculate from '../logic/calculator';
 
 const App = () => (
   <>
