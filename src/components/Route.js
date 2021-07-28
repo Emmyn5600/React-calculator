6fcb564f485844da2bb74bfccd430c7ba5b0445d
emmyn5600 <emmy6618@gmail.com>
2021-07-28 13:08:37 +0200

    Include the Navbar file inside the Route file

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
+import Navbar from './Navbar';
 import Home from './Home';
 import Quote from './Quote';
 
 const Route = () => (
 <BrowserRouter>
+<Navbar/>
  <Switch>
      <Route exact path='' component={Home}/>
      <Route exact path="/quote" component={Quote} />
