bd8424a08c71898fee37af41a0eb7ca17a38d9aa
emmyn5600 <emmy6618@gmail.com>
2021-07-29 12:02:23 +0200

    It should perform the addition

--- src/__test__/logic/operate.test.js
+++ src/__test__/logic/operate.test.js
@@ -1,8 +1,13 @@
 import operate from '../../logic/operate';
 
 describe('should perform the operation', () => {
-  it('should add two number', ()=> {
+  it('should add two numbers', ()=> {
       const result = operate('2', '2', '+');
       expect(result * 1).toEqual(4);
-  })
+  });
+
+  it('should substract two numbers', () => {
+     const result = operate('2', '2', '-');
+     expect(result * 1).toEqual(0);
+  });
 });
