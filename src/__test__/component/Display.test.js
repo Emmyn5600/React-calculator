bd588559d7dcf11f3ad4ce16c027b230b0e9f6c0
emmyn5600 <emmy6618@gmail.com>
2021-07-29 12:43:03 +0200

    It should test Display file

---
+++ src/__test__/component/Display.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Display from '../../components/Display';
+
+describe('Display', () => {
+    it('it render the display', () => {
+     const tree = renderer.create(<Display/>);
+     expect(tree).toMatchSnapshot();
+    });
+});
