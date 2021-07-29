0be466b827bd710caf5f76f2c97611d8515a976e
emmyn5600 <emmy6618@gmail.com>
2021-07-29 11:04:38 +0200

    it shouls return a divisible by 100

--- src/__test__/logic/calculate.test.js
+++ src/__test__/logic/calculate.test.js
@@ -1,9 +1,15 @@
 import calculate from '../../logic/calculator';
 
 const calc1 = {total: null, next: null, operation: null};
+const calc2 = { total: '5', next: null, operation: null };
 
 describe('calculate a method', () => {
    it('should be a function', () => {
       expect(typeof calculate).toBe('function');
    });
+});
+
+describe('should return a total divide by 100', () => {
+   const result = calculate('%', calc2);
+   expect(result.total).toEqual(0.05);
 });
